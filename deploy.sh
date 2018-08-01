88ea407fb360d174d695d7a62bffd81b74805389
Lisias T <me@lisias.net>
2018-08-01 03:00:51 -0300

    Adding automation scripts

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+source ./CONFIG.inc
+
+check() {
+	if [ ! -d "./GameData/$TARGETDIR/Plugins" ] ; then
+		rm -f "./GameData/$TARGETDIR/Plugins"
+		mkdir "./GameData/$TARGETDIR/Plugins"
+	fi
+}
+
+deploy() {
+	local DLL=$1
+
+	if [ -f "./bin/Release/$DLL.dll" ] ; then
+		cp "./bin/Release/$DLL.dll" "./GameData/$TARGETDIR/Plugins"
+		if [ -f "${KSP_DEV}/GameData/$TARGETDIR/" ] ; then
+			cp "./bin/Release/$DLL.dll" "${KSP_DEV/}GameData/$TARGETDIR/Plugins"
+		fi
+	fi
+	if [ -f "./bin/Debug/$DLL.dll" ] ; then
+		if [ -d "${KSP_DEV}/GameData/$TARGETDIR/" ] ; then
+			cp "./bin/Debug/$DLL.dll" "${KSP_DEV}GameData/$TARGETDIR/Plugins"
+		fi
+	fi
+}
+
+VERSIONFILE=$PACKAGE.version
+
+check
+cp $VERSIONFILE "./GameData/$TARGETDIR"
+deploy $PACKAGE
+
